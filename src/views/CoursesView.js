e1b63019ab69919a996aa872678186ed5e477d85
zubair-y <muhammad.yousafzai@hotmail.com>
2023-12-23 20:56:48 +0100

    Added client side navigation from course page to list page

--- src/views/CoursesView.js
+++ src/views/CoursesView.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import Link from 'next/link';
 
 const CoursesView = ({ courses = [] }) => {
   return (
     <div className="flex flex-col items-center justify-center p-4 w-full">
       <h1 className="text-3xl font-bold mb-5">Your Courses</h1>
       {courses.length > 0 ? courses.map(course => (
-        <Card key={course.id} className="mb-4 w-full md:max-w-xl transition duration-150 ease-in-out transform hover:shadow-lg hover:-translate-y-1">
-          <CardHeader>
-            <CardTitle>{course.title}</CardTitle>
-          </CardHeader>
-        </Card>
+        <Link className="mb-4 w-full md:max-w-xl transition duration-150 ease-in-out transform hover:shadow-lg hover:-translate-y-1" key={course.id} href={`/course?id=${course.id}`} passHref>
+            <Card key={course.id}>
+              <CardHeader>
+                <CardTitle>{course.title}</CardTitle>
+              </CardHeader>
+            </Card>
+        </Link>
       )) : (
         <Card>
           <CardContent>
