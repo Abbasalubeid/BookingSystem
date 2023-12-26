974fe826a4b1864f6562aab31860d607885dffe3
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-12-26 20:03:18 +0100

    Find a users reservation

--- src/models/Reservation.js
+++ src/models/Reservation.js
@@ -6,15 +6,7 @@ export default class Reservation {
     this.coopId = data.coop_id || null;
     this.sequence = data.sequence;
     this.username = data.username; // Username who made the reservation
-    this.start = new Date(data.start); // Start time of the list
-    this.interval = data.interval; // Interval between slots
-  }
-
-  // Calculate the start time of this reservation
-  calculateStartTime() {
-    const slotTime = new Date(this.start.getTime());
-    slotTime.setMinutes(slotTime.getMinutes() + this.interval * this.sequence);
-    return `${slotTime.toLocaleDateString()} ${slotTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+    
   }
 
   // Check if the reservation is cooperative (i.e., has a coopId)
