d5b432d90b5726e5c611ae80d306991cf03020e3
zubair-y <muhammad.yousafzai@hotmail.com>
2023-12-28 23:14:45 +0100

    Implemented simple Navigation bar.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from "@/components/ThemeProvider"
 import { ModeToggle } from '@/components/ModeToggle'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <html lang="en" suppressHydrationWarning>
         <head />
         <body>
+          <Navbar></Navbar>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
---
+++ src/components/Navbar.js
@@ -0,0 +1,62 @@
+"use client"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <NavigationMenu className="p-3">
+        <NavigationMenuList>
+            <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                </NavigationMenuLink>
+            </Link>
+
+            <Link href="/courses" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Courses
+                </NavigationMenuLink>
+            </Link>
+
+            <Link href="/reservations" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    My Reservations
+                </NavigationMenuLink>
+            </Link>
+
+            <Link href="/admin" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Admin
+                </NavigationMenuLink>
+            </Link>
+
+            <Link href="/login" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Log in
+                </NavigationMenuLink>
+            </Link>
+            
+            <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Log out
+                </NavigationMenuLink>
+            </Link>
+            </NavigationMenuItem>
+        </NavigationMenuList>
+    </NavigationMenu>
+
+  );
+};
+
+export default Navbar;
