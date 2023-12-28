4198fc0c58c940d3c80bb5dd90eb36e210a28fd1
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-12-28 14:18:07 +0100

    Making a reservation is now atomic using PostgreSQL's transaction control commands

--- src/app/api/reservations/route.js
+++ src/app/api/reservations/route.js
@@ -15,13 +15,34 @@ export async function POST(request) {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decoded.userId;
         const { listId, sequence, coopId } = await request.json();
-        console.log(coopId);
-        await sql`
-            INSERT INTO reservations (list_id, user_id, coop_id, sequence) VALUES (${listId}, ${userId}, ${coopId}, ${sequence});
+
+        // Begin transaction
+        await sql`BEGIN`;
+
+        // Check if the slot is already booked
+        const existingBooking = await sql`
+            SELECT * FROM reservations 
+            WHERE list_id = ${listId} AND sequence = ${sequence};
         `;
+
+        if (existingBooking.count > 0) {
+            // Slot is already booked, rollback and return error
+            await sql`ROLLBACK`;
+            throw new Error("Slot already booked");
+        }
+
+        // Insert the booking
+        await sql`
+            INSERT INTO reservations (list_id, user_id, coop_id, sequence) 
+            VALUES (${listId}, ${userId}, ${coopId}, ${sequence});
+        `;
+
+        // Commit transaction
+        await sql`COMMIT`;
+
         pusher.trigger('booking-channel', 'booking-event', {
-            message: `user ${userId} made a booking on ${listId}`
-          });
+            message: `userid ${userId} made a booking on list ${listId} with coopId ${coopId}`
+        });
 
         return new Response(JSON.stringify({ message: 'Booking successful' }), {
             status: 200,
@@ -31,6 +52,10 @@ export async function POST(request) {
         });
     } catch (error) {
         console.error('Error booking slot:', error);
+
+        // Make sure to rollback in case of any error
+        await sql`ROLLBACK`;
+
         return new Response(JSON.stringify({ error: 'Internal server error' }), {
             status: 500,
             headers: {
