68c5293bbe554bafa46e15fb225a6137f61d1302
zubair-y <muhammad.yousafzai@hotmail.com>
2023-12-30 01:32:24 +0100

    Added feedback card for admin

--- src/views/AdminDashboardView.js
+++ src/views/AdminDashboardView.js
@@ -15,11 +15,6 @@ const AdminDashboardView = () => {
         </Card>
       </Link>
 
-      {/* Användare kan endast skapas av admin 
-        KLAR
-      */}
-
-
       <Link className="mb-4 w-80 md:max-w-xl transition duration-150 ease-in-out transform hover:shadow-lg hover:-translate-y-1" href="/admin/courseAccessManagement" passHref>
         <Card>
           <CardHeader>
@@ -28,12 +23,6 @@ const AdminDashboardView = () => {
         </Card>
       </Link>
 
-      { /* 3 
-        Användare ska endast ha rättigheter på kurser som admin bestämmer och därför 
-        krävs en tabell access som länkar mellan course_id och user_id. 
-        KLAR
-      */}
-
       <Link className="mb-4 w-80 md:max-w-xl transition duration-150 ease-in-out transform hover:shadow-lg hover:-translate-y-1" href="/admin/lists" passHref>
         <Card>
           <CardHeader>
@@ -41,11 +30,7 @@ const AdminDashboardView = () => {
           </CardHeader>
         </Card>
       </Link>
-      {/* 3
-        Skapa presentationslistor.
-        Visa presentationslistor.
-        Ta bort presentationslistor.
-      */}
+
       <Link className="mb-4 w-80 md:max-w-xl transition duration-150 ease-in-out transform hover:shadow-lg hover:-translate-y-1" href="/admin/bookings" passHref>
         <Card>
           <CardHeader>
@@ -53,14 +38,17 @@ const AdminDashboardView = () => {
           </CardHeader>
         </Card>
       </Link>
+
+      <Link className="mb-4 w-80 md:max-w-xl transition duration-150 ease-in-out transform hover:shadow-lg hover:-translate-y-1" href="/admin/feedback" passHref>
+        <Card>
+          <CardHeader>
+            <CardTitle>Course Feedback</CardTitle>
+          </CardHeader>
+        </Card>
+      </Link>
       <Separator className="my-4" />
     </div>
   );
 };
-
-        /* 4
-          Admin kan lägga till bokningar för andra användare
-          Admin ta bort bokningar för andra användare
-        */
 
 export default AdminDashboardView;
