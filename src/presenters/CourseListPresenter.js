90f64fd5b2198e576e6d8dc75c3132e1d17e1881
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-01-01 15:30:50 +0100

    Admin specific functionality

--- src/presenters/CourseListPresenter.js
+++ src/presenters/CourseListPresenter.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect, Suspense } from "react";
 import List from "@/models/List";
 import CourseListView from "@/views/CourseListView";
 import ReservationDialogView from "@/views/ReservationDialogView";
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 
-const CourseListPresenter = ({ courseId }) => {
+const CourseListPresenter = ({ courseId, isAdmin }) => {
   const [listDTOs, setlistDTOs] = useState([]);
   const [listModelsMap, setListModelsMap] = useState({});
   const [error, setError] = useState(null);
@@ -17,13 +17,18 @@ const CourseListPresenter = ({ courseId }) => {
   const [teammateUsername, setTeammateUsername] = useState("");
   const [teammateError, setTeammateError] = useState("");
 
+  // Admin specific states
+  const [userOne, setUserOne] = useState("");
+  const [userTwo, setUserTwo] = useState("");
+  const [adminBookingError, setAdminBookingError] = useState("");
+
   useEffect(() => {
     fetchLists();
   }, [courseId]);
 
   useEffect(() => {
     const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-      cluster: 'eu'
+      cluster: "eu",
     });
 
     const reservationsChannel = pusher.subscribe("reservation-channel");
@@ -46,7 +51,6 @@ const CourseListPresenter = ({ courseId }) => {
       pusher.disconnect();
     };
   }, []);
-
 
   const getUserByUsername = async (username) => {
     const response = await fetch(`/api/user?username=${username}`, {
@@ -75,13 +79,15 @@ const CourseListPresenter = ({ courseId }) => {
   };
 
   const isUserRegisteredInCourse = async (userId, courseId) => {
-    const response = await fetch(`/api/access?userId=${userId}&courseId=${courseId}`)
-  
+    const response = await fetch(
+      `/api/access?userId=${userId}&courseId=${courseId}`
+    );
+
     if (!response.ok) {
       console.error("Failed to check course access");
       return false;
     }
-  
+
     const data = await response.json();
     return data.isRegistered;
   };
@@ -149,11 +155,13 @@ const CourseListPresenter = ({ courseId }) => {
         setIsBooking(false);
         return;
       }
-      
+
       coopId = coop.id;
       const isRegistered = await isUserRegisteredInCourse(coop.id, courseId);
       if (!isRegistered) {
-        setTeammateError(`${teammateUsername} is not registered in this course`);
+        setTeammateError(
+          `${teammateUsername} is not registered in this course`
+        );
         setIsBooking(false);
         return;
       }
@@ -173,30 +181,151 @@ const CourseListPresenter = ({ courseId }) => {
     const sequence = listModel.getNextSequence();
 
     try {
-      // API call to book the slot
-      const response = await fetch("/api/reservations", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ listId: listModel.id, sequence, coopId: coopId}),
-        credentials: "include",
-      });
-
-      if (response.ok) {
-        const bookingMessage = coop 
-        ? `Booking confirmed for ${user.username} and ${coop.username} at ${nextAvailableTime}`
+      await bookSlot(listModel.id, user.id, sequence, coopId);
+      const bookingMessage = coopId
+        ? `Booking confirmed for ${user.username} and ${teammateUsername} at ${nextAvailableTime}`
         : `Booking confirmed for ${user.username} at ${nextAvailableTime}`;
       setBookingConfirmation(bookingMessage);
-
-        fetchLists(); // Refresh the list data
-      } else {
-        setBookingConfirmation("Failed to book the slot");
-      }
+      fetchLists(); // Refresh the list data
     } catch (error) {
-      //TODO error UI
+      setBookingConfirmation("Failed to book the slot");
     }
 
     setIsBooking(false);
     setTeammateUsername("");
+  };
+
+  const bookSlot = async (listId, userId, sequence, coopId = null) => {
+    await fetch("/api/reservations", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ listId, userId, sequence, coopId }),
+      credentials: "include",
+    });
+  };
+
+  const handleAdminBook = async () => {
+    setIsBooking(true);
+    setAdminBookingError("");
+
+    if (userOne && userTwo && userOne === userTwo) {
+      setAdminBookingError("Error: Fill only field user when booking for one user");
+      setIsBooking(false);
+      return;
+    }
+
+    const listModel = listModelsMap[currentList.id];
+    if (!listModel) {
+      setAdminBookingError("Error: Invalid list selected.");
+      setIsBooking(false);
+      return;
+    }
+
+    // Determine the primary and secondary users
+    let primaryUser = userOne || userTwo;
+    let secondaryUser = userOne && userTwo ? userTwo : null;
+
+    // Check if at least one user is provided
+    if (!primaryUser) {
+      setAdminBookingError(
+        "Error: Please provide at least one user for booking."
+      );
+      setIsBooking(false);
+      return;
+    }
+
+    // Fetch user data for the primary and secondary users
+    const primaryUserData = await getUserByUsername(primaryUser);
+    let secondaryUserData = null;
+    if (secondaryUser) {
+      secondaryUserData = await getUserByUsername(secondaryUser);
+    }
+
+    // Check if primary user is found
+    if (!primaryUserData) {
+      setAdminBookingError(`Error: User '${primaryUser}' not found.`);
+      setIsBooking(false);
+      return;
+    }
+
+    // If secondary user is provided but not found
+    if (secondaryUser && !secondaryUserData) {
+      setAdminBookingError(`Error: User '${secondaryUser}' not found.`);
+      setIsBooking(false);
+      return;
+    }
+
+    // Check if primary and secondary users are registered in the course
+    const isPrimaryUserRegistered = await isUserRegisteredInCourse(
+      primaryUserData.id,
+      courseId
+    );
+    if (!isPrimaryUserRegistered) {
+      setAdminBookingError(
+        `Error: '${primaryUser}' is not registered in this course.`
+      );
+      setIsBooking(false);
+      return;
+    }
+
+    let isSecondaryUserRegistered = true;
+    if (secondaryUserData) {
+      isSecondaryUserRegistered = await isUserRegisteredInCourse(
+        secondaryUserData.id,
+        courseId
+      );
+      if (!isSecondaryUserRegistered) {
+        setAdminBookingError(
+          `Error: '${secondaryUser}' is not registered in this course.`
+        );
+        setIsBooking(false);
+        return;
+      }
+    }
+
+    // Check for existing bookings for both users
+    const existingBookingPrimary = listModel.userHasBooking(primaryUserData.id);
+    if (existingBookingPrimary) {
+      setAdminBookingError(
+        `Error: '${primaryUser}' already has a booking at ${existingBookingPrimary}.`
+      );
+      setIsBooking(false);
+      return;
+    }
+
+    let existingBookingSecondary = null;
+    if (secondaryUserData) {
+      existingBookingSecondary = listModel.userHasBooking(secondaryUserData.id);
+      if (existingBookingSecondary) {
+        setAdminBookingError(
+          `Error: '${secondaryUser}' already has a booking at ${existingBookingSecondary}.`
+        );
+        setIsBooking(false);
+        return;
+      }
+    }
+
+    const sequence = listModel.getNextSequence();
+    try {
+      console.log(primaryUserData);
+      await bookSlot(
+        listModel.id,
+        primaryUserData.id,
+        sequence,
+        secondaryUserData ? secondaryUserData.id : null
+      );
+      const bookingMessage = secondaryUserData
+        ? `Booking confirmed for ${primaryUser} and ${secondaryUser}`
+        : `Booking confirmed for ${primaryUser}`;
+      setBookingConfirmation(bookingMessage);
+      fetchLists();
+    } catch (error) {
+      setAdminBookingError("Failed to book the slot: " + error.message);
+    }
+
+    setIsBooking(false);
+    setUserOne("");
+    setUserTwo("");
   };
 
   const handleBadgeClick = async (list) => {
@@ -225,6 +354,9 @@ const CourseListPresenter = ({ courseId }) => {
     setLoadingListId(null);
     setTeammateUsername("");
     setTeammateError("");
+    setUserOne("");
+    setUserTwo("");
+    setAdminBookingError("");
   };
 
   return (
@@ -240,11 +372,17 @@ const CourseListPresenter = ({ courseId }) => {
         showDialog={showDialog}
         onCloseDialog={handleCloseDialog}
         nextAvailableTime={nextAvailableTime}
-        onBook={handleBook}
         isBooking={isBooking}
         bookingConfirmation={bookingConfirmation}
         setTeammateUsername={setTeammateUsername}
         teammateError={teammateError}
+        onBook={isAdmin ? handleAdminBook : handleBook}
+        isAdmin={isAdmin}
+        userOne={userOne}
+        setUserOne={setUserOne}
+        userTwo={userTwo}
+        setUserTwo={setUserTwo}
+        adminBookingError={adminBookingError}
       ></ReservationDialogView>
     </>
   );
